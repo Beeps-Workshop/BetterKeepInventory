0490ae14bafcf5c72d4e505cc32a7fe2352492a5
Beepsterr <git@beeps.email>
2026-08-17 20:33:55 +0200

    remove dead grace timer placeholders

--- plugin/src/main/java/com/beepsterr/betterkeepinventory/Depends/BetterKeepInventoryPlaceholderExpansion.java
+++ plugin/src/main/java/com/beepsterr/betterkeepinventory/Depends/BetterKeepInventoryPlaceholderExpansion.java
@@ -23,19 +23,6 @@ public class BetterKeepInventoryPlaceholderExpansion extends PlaceholderExpansio
 
     @Override
     public String onPlaceholderRequest(org.bukkit.entity.Player player, @NotNull String identifier) {
-
-        identifier = identifier.toLowerCase();
-
-        // Seconds left on grace
-        if (identifier.equals("grace_timer_seconds")) {
-            return "0"; // TODO: Re-Implement this
-        }
-
-        // If grace is active
-        if(identifier.equals("grace_timer_active")) {
-            return "false"; // TODO: Re-Implement this
-        }
-
         return null;
     }
 }
